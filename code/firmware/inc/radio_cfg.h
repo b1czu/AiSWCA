93c25212a87f62f81e480e38d7d9776c5d7cc463
PRoguski <piotr8roguski@gmail.com>
2016-09-15 20:58:58 +0200

    Added radio config file (predefined resistance value)

---
+++ code/firmware/inc/radio_cfg.h
@@ -0,0 +1,26 @@
+/*
+ * radio_cfg.h
+ *
+ *  Created on: Sep 15, 2016
+ *      Author: piotr
+ */
+
+#ifndef INC_RADIO_CFG_H_
+#define INC_RADIO_CFG_H_
+
+#define PIONEER_RADIO
+
+#ifdef PIONEER_RADIO
+ 
+ #define RADIO_BAND_BTN 			20
+ #define RADIO_VOLUME_DOWN_BTN 		100
+ #define RADIO_VOLUME_UP_BTN 		165
+ #define RADIO_TUNE_DOWN_BTN 		190
+ #define RADIO_TUNE_UP_BTN 			215
+ #define RADIO_DISPLAY_BTN 			227
+ #define RADIO_ATT_BTN 				238
+ #define RADIO_SOURCE_BTN 			250
+
+#endif
+
+#endif /* INC_RADIO_CFG_H_ */
